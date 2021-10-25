7ac0c22d3219c8e33132d577b9358fb0b7fd3dbf
Sk Imran Sabuj <sabujx9@gmail.com>
2021-10-25 21:20:32 +0600

    first node express commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+const port = 5000;
+const users = [
+    { id: 0, name: 'Shabana', email: 'Shabana@gmail.com', phone: '01788888888' },
+    { id: 1, name: 'Shabnoor', email: 'Shabnoor@gmail.com', phone: '01788888888' },
+    { id: 2, name: 'Shrabonti', email: 'Shrabonti@gmail.com', phone: '01788888888' },
+    { id: 3, name: 'Suchorita', email: 'Suchorita@gmail.com', phone: '01788888888' },
+    { id: 4, name: 'Soniya', email: 'Soniya@gmail.com', phone: '01788888888' },
+    { id: 5, name: 'Susmita', email: 'Susmita@gmail.com', phone: '01788888888' },
+];
+app.get('/', (req, res) => {
+
+    res.send('Hello Node')
+});
+app.get('/users', (req, res) => {
+    const search = req.query.search;
+    if (search) {
+        const searchResult = users.filter(user => user.name.toLowerCase().includes(search));
+        res.send(searchResult)
+    }
+    else {
+        res.send(users)
+    }
+});
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    newUser.id = users.length;
+    users.push(newUser);
+    console.log('Hitting the post', req.body);
+    res.json(newUser);
+})
+app.get('/users/:id', (req, res) => {
+    console.log(req.params.id)
+    const user = users[req.params.id];
+    res.send(user)
+})
+
+
+app.listen(port, () => {
+    console.log('Listining Port', port)
+})
